db05412768feccdf0bece9150fb25db9f9b774dc
diepsang13 <diepqsang13@gmail.com>
2024-08-17 20:20:42 +0700

    init data structure content

---
+++ data_structures/linked_list.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtHead(self, data):
+        new_node = Node(data)
+        if self.head:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            self.head = new_node
+
+    def insertAtTail(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+
+        curr_node = self.head
+        while curr_node.next:
+            curr_node = curr_node.next
+        curr_node.next = new_node
+
+    def insertAtIndex(self, data, index):
+        if index == 0:
+            self.insertAtHead(data)
+            return
+
+        possition = 0
+        curr_node = self.head
+        while curr_node and possition + 1 != index:
+            possition += 1
+            curr_node = curr_node.next
+
+        if not curr_node:
+            raise Exception('Index not present')
+
+        new_node = Node(data)
+        new_node.next = curr_node.next
+        curr_node.next = new_node
+
+    def check_linked_list(self):
+        size = 0
+        data_list = []
+
+        if self.head:
+            curr_node = self.head
+            while (curr_node):
+                size += 1
+                data_list.append(curr_node.data)
+                curr_node = curr_node.next
+
+        print(f'size: {size}')
+        print(f'data: {' -> '.join(map(str, data_list))}')
+
+
+llist = LinkList()
+llist.insertAtHead(1)
+llist.insertAtHead(2)
+llist.insertAtTail(7)
+llist.insertAtTail(5)
+llist.check_linked_list()
+
+llist.insertAtIndex(11, 2)
+llist.insertAtIndex(13, 3)
+llist.check_linked_list()
---
+++ data_structures/tree.py
@@ -0,0 +1,74 @@
+class TreeNode:
+    def __init__(self, data) -> None:
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data) -> None:
+        # insert to root
+        if self.data is None:
+            self.data = data
+
+        # insert to left node
+        elif data < self.data:
+            if self.left is None:
+                self.left = TreeNode(data)
+            else:
+                self.left.insert(data)
+
+        # insert to right node
+        elif data > self.data:
+            if self.right is None:
+                self.right = TreeNode(data)
+            else:
+                self.right.insert(data)
+
+    def check(self) -> None:
+        if self.left:
+            self.left.check()
+
+        print(self.data)
+
+        if self.right:
+            self.right.check()
+
+    def in_order_traversal(self, root):
+        iter: list = []
+        if root:
+            iter = self.in_order_traversal(root.left)
+            iter.append(root.data)
+            iter += self.in_order_traversal(root.right)
+        return iter
+
+    def pre_order_tranversal(self, root):
+        iter: list = []
+        if root:
+            iter.append(root.data)
+            iter += self.pre_order_tranversal(root.left)
+            iter += self.pre_order_tranversal(root.right)
+        return iter
+
+    def post_order_tranversal(self, root):
+        iter: list = []
+        if root:
+            iter = self.post_order_tranversal(root.left)
+            iter += self.post_order_tranversal(root.right)
+            iter.append(root.data)
+        return iter
+
+
+def main():
+    root: TreeNode = TreeNode(27)
+    root.insert(14)
+    root.insert(35)
+    root.insert(10)
+    root.insert(19)
+    root.insert(31)
+    root.insert(42)
+    print(root.in_order_traversal(root))
+    print(root.pre_order_tranversal(root))
+    print(root.post_order_tranversal(root))
+
+
+if __name__ == '__main__':
+    main()
